511ce471cfeb5742031a8cbbe7f44309efb838e4
DanTerra45 <daniel.terrazas@ucb.edu.bo>
2023-06-01 23:35:44 -0400

    Final fixes to main.js (allow multiple entries using pools)

--- base_exp_v3.sql
+++ base_exp_v3.sql
@@ -80,3 +80,82 @@ ALTER TABLE `travel` ADD FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
 ALTER TABLE `travel` ADD FOREIGN KEY (`driverID`) REFERENCES `driver` (`driverID`);
 
 ALTER TABLE `travel` ADD FOREIGN KEY (`vehicleID`) REFERENCES `vehicle` (`vehicleID`);
+
+/*
+-- Creación para SQL --
+
+CREATE TABLE user (
+  userID int PRIMARY KEY,
+  firstName varchar(255),
+  lastName varchar(255),
+  email varchar(255) CHECK (email LIKE '%@gmail.com' OR email LIKE '%@hotmail.com' OR email LIKE '%@outlook.com'),
+  country varchar(255),
+  birthday varchar(255),
+  city varchar(255),
+  phone int CHECK (phone <= 99999999)
+);
+
+CREATE TABLE driver (
+  driverID int PRIMARY KEY,
+  firstName varchar(255),
+  lastName varchar(255),
+  email varchar(255) CHECK (email LIKE '%@gmail.com' OR email LIKE '%@hotmail.com' OR email LIKE '%@outlook.com'),
+  country varchar(255),
+  birthday varchar(255),
+  city varchar(255),
+  phone int CHECK (phone <= 99999999)
+);
+
+CREATE TABLE vehicle (
+  vehicleID int PRIMARY KEY,
+  driverID int,
+  plate varchar(255),
+  vehicleModel varchar(255),
+  vehicleManufacturedDate varchar(255),
+  FOREIGN KEY (driverID) REFERENCES driver(driverID)
+);
+
+CREATE TABLE payment (
+  paymentID int PRIMARY KEY,
+  userID int,
+  ammountPay varchar(255),
+  datePay varchar(255),
+  typePay int CHECK (typePay <= 2),
+  FOREIGN KEY (userID) REFERENCES user(userID)
+);
+
+CREATE TABLE promotion (
+  promotionID int PRIMARY KEY,
+  userID int,
+  promoCode varchar(255),
+  datePromo varchar(255),
+  discountPromo varchar(255),
+  descriptionPromo varchar(255),
+  FOREIGN KEY (userID) REFERENCES user(userID)
+);
+
+CREATE TABLE rating (
+  ratingID int PRIMARY KEY,
+  userID int,
+  driverID int,
+  rating int CHECK (rating <= 5),
+  ratingDescription varchar(255),
+  FOREIGN KEY (userID) REFERENCES user(userID),
+  FOREIGN KEY (driverID) REFERENCES driver(driverID)
+);
+
+CREATE TABLE travel (
+  travelID int PRIMARY KEY,
+  userID int,
+  driverID int,
+  vehicleID int,
+  originPoint varchar(255),
+  destination varchar(255),
+  dateTrip varchar(255),
+  tripDuration varchar(255),
+  priceTrip varchar(255),
+  FOREIGN KEY (userID) REFERENCES user(userID),
+  FOREIGN KEY (driverID) REFERENCES driver(driverID),
+  FOREIGN KEY (vehicleID) REFERENCES vehicle(vehicleID)
+);
+*/
--- server/main.js
+++ server/main.js
@@ -19,12 +19,10 @@ app.get('/', (req, res) => {
 app.post('/user',(req, res) => {
   const body = req.body;
   const query = `INSERT INTO user(userID, firstName, lastName, email, country, birthday, city, phone) VALUES ('${body.userID}', '${body.firstName}', '${body.lastName}', '${body.email}', '${body.country}', '${body.birthday}', '${body.city}', '${body.phone}');`;
-  //connection.connect();
-  connection.query( query, (err, rows, fields) => {
+  pool.query( query, (err, result) => {
     if (err) throw err
     console.log('1 record inserted');
   })
-  //connection.end();
   res.send('1 record inserted');
 })
 
@@ -32,12 +30,10 @@ app.post('/user',(req, res) => {
 app.post('/driver',(req, res) => {
   const body = req.body;
   const query = `INSERT INTO driver(driverID, firstName, lastName, email, country, birthday, city, phone) VALUES ('${body.driverID}', '${body.firstName}', '${body.lastName}', '${body.email}', '${body.country}', '${body.birthday}', '${body.city}', '${body.phone}');`;
-  //connection.connect();
-  connection.query( query, (err, rows, fields) => {
+  pool.query( query, (err, result) => {
     if (err) throw err
     console.log('1 record inserted');
   })
-  //connection.end();
   res.send('1 record inserted');
 })
 
@@ -45,12 +41,10 @@ app.post('/driver',(req, res) => {
 app.post('/vehicle',(req, res) => {
   const body = req.body;
   const query = `INSERT INTO vehicle(vehicleID, driverID, plate, vehicleModel, vehicleManufacturedDate) VALUES ('${body.vehicleID}', '${body.driverID}', '${body.plate}', '${body.vehicleModel}', '${body.vehicleManufacturedDate}');`;
-  //connection.connect();
-  connection.query( query, (err, rows, fields) => {
+  pool.query( query, (err, result) => {
     if (err) throw err
     console.log('1 record inserted');
   })
-  //connection.end();
   res.send('1 record inserted');
 })
 
@@ -58,12 +52,10 @@ app.post('/vehicle',(req, res) => {
 app.post('/payment',(req, res) => {
   const body = req.body;
   const query = `INSERT INTO payment(paymentID, userID, ammountPay, datePay, typePay) VALUES ('${body.paymentID}', '${body.userID}', '${body.ammountPay}', '${body.datePay}', '${body.typePay}');`;
-  //connection.connect();
-  connection.query( query, (err, rows, fields) => {
+  pool.query( query, (err, result) => {
     if (err) throw err
     console.log('1 record inserted');
   })
-  //connection.end();
   res.send('1 record inserted');
 })
 
@@ -71,12 +63,10 @@ app.post('/payment',(req, res) => {
 app.post('/promotion',(req, res) => {
   const body = req.body;
   const query = `INSERT INTO promotion(promotionID, userID, promoCode, datePromo, discountPromo, descriptionPromo) VALUES ('${body.promotionID}', '${body.userID}', '${body.promoCode}', '${body.datePromo}', '${body.discountPromo}', '${body.descriptionPromo}');`;
-  //connection.connect();
-  connection.query( query, (err, rows, fields) => {
+  pool.query( query, (err, result) => {
     if (err) throw err
     console.log('1 record inserted');
   })
-  //connection.end();
   res.send('1 record inserted');
 })
 
@@ -84,12 +74,10 @@ app.post('/promotion',(req, res) => {
 app.post('/rating',(req, res) => {
   const body = req.body;
   const query = `INSERT INTO rating(ratingID, userID, driverID, rating, ratingDescription) VALUES ('${body.ratingID}', '${body.userID}', '${body.driverID}', '${body.rating}', '${body.ratingDescription}');`;
-  //connection.connect();
-  connection.query( query, (err, rows, fields) => {
+  pool.query( query, (err, result) => {
     if (err) throw err
     console.log('1 record inserted');
   })
-  //connection.end();
   res.send('1 record inserted');
 })
 
@@ -97,12 +85,10 @@ app.post('/rating',(req, res) => {
 app.post('/travel',(req, res) => {
   const body = req.body;
   const query = `INSERT INTO travel(travelID, userID, driverID, vehicleID, originPoint, destination, dateTrip, tripDuration, priceTrip) VALUES ('${body.travelID}', '${body.userID}', '${body.driverID}', '${body.vehicleID}', '${body.originPoint}', '${body.destination}', '${body.dateTrip}', '${body.tripDuration}', '${body.priceTrip}');`;
-  //connection.connect();
-  connection.query( query, (err, rows, fields) => {
+  pool.query( query, (err, result) => {
     if (err) throw err
     console.log('1 record inserted');
   })
-  //connection.end();
   res.send('1 record inserted');
 })
 
@@ -110,12 +96,10 @@ app.listen(port, () => {
   console.log(`Project sample is running on: ${port}`)
 })
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'localhost',
   port: '3306',
   user: 'root',
   password: 'root123',
   database: 'db1'
 })
-
-connection.connect();
